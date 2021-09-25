82aa42eaf6964b0a8b84ac2a5fc6b5a99caab786
heros20 <heros40@hotmail.fr>
2021-09-25 12:01:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Bienvenue sur mon premier projet React!</h1>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Bienvenue sur mon premier projet React!</h1>
+//     </div>
+//   );
+// }
+
+// class App extends Component {
+//    render() {
+//      return (
+//       <div className="App">
+//         <h1>Bienvenue sur mon premier projet React!</h1>
+//       </div>
+//     );
+//    }
+// }
+export default App;
